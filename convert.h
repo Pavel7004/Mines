c0e40ee84e7745716b35a57ceb959eacbf87fe11
Pavel7004 <kovalev5690@gmail.com>
2019-03-20 18:34:28 +1000

    initial commit

---
+++ convert.h
@@ -0,0 +1,11 @@
+#ifndef __CONVERT__
+#define __CONVERT__
+
+#include <string>
+
+using namespace std;
+
+// функция конвертирует числом в типе string в тип short
+short convert_to_short(const string &num);
+
+#endif
